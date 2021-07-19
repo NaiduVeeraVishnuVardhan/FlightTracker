c153d0f073d55a74035562e8ff2d93ce78286c24
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-07-19 19:41:53 +1000

    3DModel

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
